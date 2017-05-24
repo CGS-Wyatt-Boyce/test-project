28c989a116bfc2269224e3182f82df5824157fb0
Wyatt A. Boyce <Wyatt@KMZFX-XOXZ.local>
2017-05-24 11:35:11 +1000

    commit 4

--- Test Project/ViewController.swift
+++ Test Project/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         print("Hello I am Death")
+        print("master")
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
