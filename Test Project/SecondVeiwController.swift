6878af064c4e22c7b65763c7a995b915ad7d8d82
Wyatt A. Boyce <Wyatt@KMZFX-XOXZ.local>
2017-06-19 09:35:01 +1000

    another commit

---
+++ Test Project/SecondVeiwController.swift
@@ -0,0 +1,40 @@
+//
+//  SecondVeiwController.swift
+//  Test Project
+//
+//  Created by Wyatt A. Boyce on 6/19/17.
+//  Copyright © 2017 Wyatt A. Boyce. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        
+        
+        print("die mf")
+        
+        
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    }
+    
+   
+
+
+
+
+
+
+
