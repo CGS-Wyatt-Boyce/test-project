5cf846b40b89a14af254e9eabfced09a5dc77f48
Wyatt A. Boyce <Wyatt@KMZFX-XOXZ.local>
2017-06-20 07:59:06 +1000

    commit of second

--- Test Project/SecondVeiwController.swift
+++ Test Project/SecondVeiwController.swift
@@ -15,6 +15,10 @@ class SecondViewController: UIViewController {
         
         print("die mf")
         
+        func deathAgain() {
+            print("we all died all over again")
+        }
+        
         
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
